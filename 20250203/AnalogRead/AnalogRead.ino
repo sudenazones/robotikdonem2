ea9f6088a61ef2725ae9199149d5cf7614778c69
sudenazones <sudehnk4@gmail.com>
2025-02-04 13:55:00 +0300

    Add files via upload

---
+++ 20250203/AnalogRead/AnalogRead.ino
@@ -0,0 +1,22 @@
+/*
+  AnalogReadSerial
+
+  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
+*/
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  delay(1);        
+}
